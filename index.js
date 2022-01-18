76b6d393118d8c9e3058eca275371cd7841167be
iceyShardZ <Ynonn1000@gmail.com>
2022-01-18 21:01:55 +0200

    11
    1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import App2 from './App2';
+import App from './src/App';
+import App2 from './src/App2';
 
 ReactDOM.render(
   <React.StrictMode>
